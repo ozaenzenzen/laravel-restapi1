fcb01d8069035a4044f5e486ad01cd2a5c11ed3b
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-27 00:51:55 +0700

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('v1/posts/store', [TestController::class, 'store']);
+
+Route::get('/v1/posts', [TestController::class, 'index']);
+
+Route::get('/v1/posts/{id?}', [TestController::class, 'show']);
+
+Route::post('/v1/posts/update', [TestController::class, 'update']);
+
+Route::delete('/v1/posts/{id?}', [TestController::class, 'destroy']);
